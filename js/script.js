ae2c0e2d09b68e21a481f81d5f34d6302181e5f2
Manoj Somanna <manoj9901811@gmail.com>
2026-08-07 12:58:38 +0530

    first commit

--- js/script.js
+++ js/script.js
@@ -263,3 +263,35 @@ card.style.transform=`translateY(${Math.sin(angle+index)*3}px)`;
 });
 
 },40);
+
+/*======================================
+        MOBILE MENU
+======================================*/
+
+const menuBtn = document.querySelector(".menu-btn");
+
+const mobileMenu = document.querySelector(".mobile-menu");
+
+const closeMenu = document.querySelector(".close-menu");
+
+menuBtn.addEventListener("click", () => {
+
+    mobileMenu.classList.add("active");
+
+});
+
+closeMenu.addEventListener("click", () => {
+
+    mobileMenu.classList.remove("active");
+
+});
+
+document.querySelectorAll(".mobile-menu a").forEach(link => {
+
+    link.addEventListener("click", () => {
+
+        mobileMenu.classList.remove("active");
+
+    });
+
+});
